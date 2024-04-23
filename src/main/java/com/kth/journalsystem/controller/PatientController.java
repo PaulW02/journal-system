28f3d4f2bdf4304d1d6b4ffb123f3b7e201b19d1
PaulW02 <pslwi@kth.se>
2024-04-23 14:48:26 +0200

    First commit

--- src/main/java/com/kth/journalsystem/controller/PatientController.java
+++ src/main/java/com/kth/journalsystem/controller/PatientController.java
@@ -1,7 +1,5 @@
 package com.kth.journalsystem.controller;
 
-import com.kth.journalsystem.domain.Patient;
-import com.kth.journalsystem.dto.OrderDTO;
 import com.kth.journalsystem.dto.PatientDTO;
 import com.kth.journalsystem.service.producer.PatientEventProducer;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,24 +15,10 @@ public class PatientController {
     @Autowired
     private PatientEventProducer patientEventProducer;
 
-    @PostMapping("/orders")
-    public ResponseEntity<String> createOrder(@RequestBody OrderDTO orderEvent) {
-        // Process the received order event and send it to Kafka
-        patientEventProducer.sendOrderEvent(orderEvent);
-        return ResponseEntity.ok("Order created successfully");
-    }
-
     @PostMapping("/")
     public ResponseEntity<PatientDTO> createPatient(@RequestBody PatientDTO patient) {
         patientEventProducer.sendCreatePatientEvent(patient);
         PatientDTO createdPatientDTO = new PatientDTO(patient.getId(), patient.getFirstName(), patient.getLastName(), patient.getAge());
         return ResponseEntity.status(HttpStatus.CREATED).body(createdPatientDTO);
-    }
-
-    @PostMapping("/order")
-    public ResponseEntity<String> createOrderFixed() {
-        // Process the received order event and send it to Kafka
-        patientEventProducer.sendOrderEvent(new OrderDTO());
-        return ResponseEntity.ok("Order created successfully");
     }
 }
--- src/main/java/com/kth/journalsystem/service/consumer/PatientEventConsumer.java
+++ src/main/java/com/kth/journalsystem/service/consumer/PatientEventConsumer.java
@@ -8,21 +8,6 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class PatientEventConsumer {
-
-    @KafkaListener(topics = "order_events", groupId = "order_group")
-    public void handleOrderEvent(OrderDTO orderEvent) {
-        // Process the order event, e.g., store it in the database
-        System.out.println("Received Order Event: " + orderEvent);
-        // Implement the logic for order processing and updating inventory
-    }
-
-    @KafkaListener(topics = "payment_events", groupId = "payment_group")
-    public void handlePaymentEvent(PaymentDTO paymentDTO) {
-        // Process the order event, e.g., store it in the database
-        System.out.println("Received Payment Event ORDDERR: " + paymentDTO);
-        // Implement the logic for order processing and updating inventory
-    }
-
     @KafkaListener(topics = "create_patient_event", groupId = "patient_group")
     public void handleCreatePatientEvent(PatientDTO patient) {
         // Process the order event, e.g., store it in the database
